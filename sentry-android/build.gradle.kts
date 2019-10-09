a0171047c825089d5aac120f50736589d7333fa7
Manoel Aranda Neto <maranda@sentry.io>
2019-10-09 15:26:52 +0200

    rollback consumeManifest and set robolectric to use sdk 21

--- sentry-android/build.gradle.kts
+++ sentry-android/build.gradle.kts
@@ -26,7 +26,7 @@ android {
             isMinifyEnabled = false
         }
         getByName("release") {
-            consumerProguardFiles("proguard-rules.pro")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 
