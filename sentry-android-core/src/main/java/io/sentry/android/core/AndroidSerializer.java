df176b3541e88de61bfcd85993cc43ec25fcdd27
Lukas Krejci <lkrejci@redhat.com>
2019-10-13 23:11:32 +0200

    Adapth AndroidSerializer#serialize to the new signature.

--- sentry-android-core/src/main/java/io/sentry/android/core/AndroidSerializer.java
+++ sentry-android-core/src/main/java/io/sentry/android/core/AndroidSerializer.java
@@ -7,6 +7,8 @@ import io.sentry.core.ISerializer;
 import io.sentry.core.SentryEnvelope;
 import io.sentry.core.SentryEvent;
 import io.sentry.core.protocol.SentryId;
+
+import java.io.Writer;
 import java.util.Date;
 
 public class AndroidSerializer implements ISerializer {
@@ -32,7 +34,7 @@ public class AndroidSerializer implements ISerializer {
   }
 
   @Override
-  public String serialize(SentryEvent event) {
-    return gson.toJson(event);
+  public void serialize(SentryEvent event, Writer writer) {
+    gson.toJson(event, SentryEvent.class, writer);
   }
 }
