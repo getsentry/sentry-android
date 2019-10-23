a98da129775f09da906472c33f7b28a340fb1d58
Manoel Aranda Neto <maranda@sentry.io>
2019-10-23 14:44:28 +0200

    WIP

--- sentry-core/src/main/java/io/sentry/core/MainEventProcessor.java
+++ sentry-core/src/main/java/io/sentry/core/MainEventProcessor.java
@@ -1,6 +1,11 @@
 package io.sentry.core;
 
+import io.sentry.core.protocol.Message;
+import io.sentry.core.protocol.SentryException;
 import io.sentry.core.util.Objects;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainEventProcessor implements EventProcessor {
 
@@ -12,6 +17,28 @@ public class MainEventProcessor implements EventProcessor {
 
   @Override
   public SentryEvent process(SentryEvent event) {
+    Throwable throwable = event.getThrowable();
+    if (throwable != null) {
+
+      if (event.getMessage() == null) {
+        event.setMessage(getMessage(throwable));
+      }
+
+      event.setExceptions(getExceptions(throwable));
+    }
+
     return event;
+  }
+
+  private Message getMessage(Throwable throwable) {
+    Message message = new Message();
+    message.setFormatted(throwable.getMessage());
+    return message;
+  }
+
+  private List<SentryException> getExceptions(Throwable throwable) {
+    List<SentryException> exceptions = new ArrayList<>();
+
+    return exceptions;
   }
 }
