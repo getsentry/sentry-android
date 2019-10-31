1dc0439d84d04d9eb6abbe45d232f34e4680782f
Manoel Aranda Neto <maranda@sentry.io>
2019-10-31 13:44:58 +0100

    spotless

--- sentry-sample/src/main/java/io/sentry/sample/MyApplication.java
+++ sentry-sample/src/main/java/io/sentry/sample/MyApplication.java
@@ -20,14 +20,13 @@ public class MyApplication extends Application {
     // SentryAndroid.init(this);
   }
 
-
   private void districtMode() {
     //    https://developer.android.com/reference/android/os/StrictMode
     //    StrictMode is a developer tool which detects things you might be doing by accident and
     //    brings them to your attention so you can fix them.
     if (BuildConfig.DEBUG) {
       StrictMode.setThreadPolicy(
-        new StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build());
+          new StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build());
 
       StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().penaltyLog().build());
     }
