c2ea8e680eb484622eb8450b90fb4c2b53a2f7cb
Manoel Aranda Neto <marandaneto@gmail.com>
2019-11-06 11:49:43 +0100

    added class container for SentryOptions

--- sentry-android-core/src/main/java/io/sentry/android/core/SentryAndroid.java
+++ sentry-android-core/src/main/java/io/sentry/android/core/SentryAndroid.java
@@ -2,9 +2,11 @@ package io.sentry.android.core;
 
 import android.content.Context;
 import io.sentry.core.ILogger;
+import io.sentry.core.OptionsContainer;
 import io.sentry.core.Sentry;
-import java.lang.reflect.InvocationTargetException;
 import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.InvocationTargetException;
 
 /** Sentry initialization class */
 public final class SentryAndroid {
@@ -23,7 +25,7 @@ public final class SentryAndroid {
   static void init(@NotNull final Context context, @NotNull ILogger logger) {
     try {
       Sentry.init(
-          SentryAndroidOptions.class,
+          OptionsContainer.create(SentryAndroidOptions.class),
           options -> AndroidOptionsInitializer.init(options, context, logger));
     } catch (IllegalAccessException e) {
       // This is awful. Should we have this all on the interface and let the caller deal with these?
