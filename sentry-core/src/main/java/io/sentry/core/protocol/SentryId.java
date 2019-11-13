4089f2d05c85989462b9eba164aa9359875c7955
Bruno Garcia <bruno@brunogarcia.com>
2019-11-13 14:46:53 -0500

    fix: envelope header event id

--- sentry-core/src/main/java/io/sentry/core/protocol/SentryId.java
+++ sentry-core/src/main/java/io/sentry/core/protocol/SentryId.java
@@ -27,6 +27,19 @@ public final class SentryId {
     return uuid.toString().replace("-", "");
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    SentryId sentryId = (SentryId) o;
+    return uuid.equals(sentryId.uuid);
+  }
+
+  @Override
+  public int hashCode() {
+    return uuid.hashCode();
+  }
+
   private UUID fromStringSentryId(String sentryIdString) {
     if (sentryIdString == null) {
       return null;
