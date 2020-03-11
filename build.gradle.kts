20b63ae2a0a06d3f85c88e372dd31736c186330d
Manoel Aranda Neto <marandaneto@gmail.com>
2020-03-11 09:16:20 +0100

    fix lint check

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,9 +54,9 @@ allprojects {
                 TestLogEvent.FAILED)
             dependsOn("cleanTest")
         }
-//        withType<JavaCompile> {
-//            options.compilerArgs.addAll(arrayOf("-Xlint:all", "-Werror", "-Xlint:-classfile"))
-//        }
+        withType<JavaCompile> {
+            options.compilerArgs.addAll(arrayOf("-Xlint:all", "-Werror", "-Xlint:-classfile"))
+        }
     }
 }
 
