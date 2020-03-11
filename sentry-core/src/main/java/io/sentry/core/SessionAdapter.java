b293d92e91853c465d23ea7401f914dd88b4340d
Manoel Aranda Neto <marandaneto@gmail.com>
2020-03-11 09:12:01 +0100

    fix unit tests

--- sentry-core/src/main/java/io/sentry/core/SessionAdapter.java
+++ sentry-core/src/main/java/io/sentry/core/SessionAdapter.java
@@ -115,6 +115,6 @@ public final class SessionAdapter extends TypeAdapter<Session> {
       return str;
     }
 
-    return str.substring(0, 1).toUpperCase() + str.substring(1);
+    return str.substring(0, 1).toUpperCase(Locale.ROOT) + str.substring(1);
   }
 }
