959e5d08ea21574b3bf7569b01df01b1498b9c5e
Manoel Aranda Neto <marandaneto@gmail.com>
2020-04-21 09:12:37 +0200

    bump version name and code

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -50,10 +50,10 @@ object Config {
         val SENTRY_CLIENT_NAME = "sentry.java.android"
         val group = "io.sentry"
 //        TODO: change version to publish new version
-        val version = "2.0.2"
+        val version = "2.0.3"
         val description = "SDK for sentry.io"
 //        TODO: change version code to publish new version, follow the pattern of `version`
-        val buildVersionCode = 20018
+        val buildVersionCode = 20023
         val website = "https://sentry.io"
         val userOrg = "getsentry"
         val repoName = "sentry-android"
