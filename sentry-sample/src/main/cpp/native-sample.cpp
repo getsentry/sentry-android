46798e7f58a600e8c631d08c7ca3d48ef78baab1
Manoel Aranda Neto <marandaneto@gmail.com>
2020-05-30 13:15:24 +0200

    just to reproduce a bug

--- sentry-sample/src/main/cpp/native-sample.cpp
+++ sentry-sample/src/main/cpp/native-sample.cpp
@@ -14,10 +14,24 @@ JNIEXPORT void JNICALL Java_io_sentry_sample_NativeSample_crash(JNIEnv *env, jcl
 
 JNIEXPORT void JNICALL Java_io_sentry_sample_NativeSample_message(JNIEnv *env, jclass cls) {
     __android_log_print(ANDROID_LOG_WARN, TAG, "Sending message.");
+
+    jclass clazz = env->FindClass("io/sentry/sample/NativeSample");
+    jmethodID method = env->GetStaticMethodID(clazz, "test",
+                                              "()Ljava/lang/String;");
+    jstring result = (jstring) env->CallStaticObjectMethod(clazz, method);
+    // if I do a NPE check, it bails out and it works
+//    if (result == NULL) {
+//        return;
+//    }
+
+// lets keep running to segfault as result is NULL
+
+    const char *nativeString = env->GetStringUTFChars(result, 0);
+
     sentry_value_t event = sentry_value_new_message_event(
             /*   level */ SENTRY_LEVEL_INFO,
             /*  logger */ "custom",
-            /* message */ "It works!"
+            /* message */ nativeString
     );
     sentry_capture_event(event);
 }
--- sentry-sample/src/main/java/io/sentry/sample/NativeSample.java
+++ sentry-sample/src/main/java/io/sentry/sample/NativeSample.java
@@ -1,5 +1,10 @@
 package io.sentry.sample;
 
+import android.annotation.SuppressLint;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class NativeSample {
   public static native void crash();
 
@@ -7,5 +12,19 @@ public class NativeSample {
 
   static {
     System.loadLibrary("native-sample");
+  }
+
+
+  @SuppressLint("NewApi")
+  public static String test() {
+    Map<String, String> maps = new HashMap<>();
+    maps.put("test", "test");
+
+    // RequiresApi API 24, so run on <= 23 (max Android 6) to simulate a NoClassDefFoundError, which is caught by UncaughtExceptionHandlerIntegration.uncaughtException
+    maps.forEach((s, s2) -> {
+      System.out.println("s = " + s + " s2 " + s2);
+    });
+
+    return "test"; // (jstring result) will be null anyway as forEach throws NoClassDefFoundError
   }
 }
