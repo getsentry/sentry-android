c4edcc5eb4eccc1cfcf7554ab9f2a14cdb50ad17
Manoel Aranda Neto <marandaneto@gmail.com>
2020-07-03 14:31:47 +0200

    fix: contexts serializer avoids reflection and fixes desugaring issue

---
+++ sentry-android-core/src/main/java/io/sentry/android/core/adapters/ContextsSerializerAdapter.java
@@ -0,0 +1,41 @@
+package io.sentry.android.core.adapters;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import io.sentry.core.ILogger;
+import io.sentry.core.SentryLevel;
+import io.sentry.core.protocol.Contexts;
+import java.lang.reflect.Type;
+import java.util.Map;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
+
+@ApiStatus.Internal
+public final class ContextsSerializerAdapter implements JsonSerializer<Contexts> {
+
+  private final @NotNull ILogger logger;
+
+  public ContextsSerializerAdapter(@NotNull final ILogger logger) {
+    this.logger = logger;
+  }
+
+  @Override
+  public JsonElement serialize(Contexts src, Type typeOfSrc, JsonSerializationContext context) {
+    if (src == null) {
+      return null;
+    }
+
+    final JsonObject object = new JsonObject();
+    for (final Map.Entry<String, Object> entry : src.entrySet()) {
+      final JsonElement element = context.serialize(entry.getValue(), Object.class);
+      if (element != null) {
+        object.add(entry.getKey(), element);
+      } else {
+        logger.log(SentryLevel.INFO, "%s context key isn't serializable.");
+      }
+    }
+    return object;
+  }
+}
