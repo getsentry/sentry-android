41b43cc564c32bbd69ceb31324c4ca5cf856cb55
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2020-07-31 00:07:30 +0200

    Polish.

--- sentry-core/src/main/java/io/sentry/core/protocol/App.java
+++ sentry-core/src/main/java/io/sentry/core/protocol/App.java
@@ -8,7 +8,7 @@ import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.TestOnly;
 
-public final class App implements Cloneable, IUnknownPropertiesConsumer {
+public final class App implements IUnknownPropertiesConsumer, Cloneable {
   public static final String TYPE = "app";
 
   private String appIdentifier;
