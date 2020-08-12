01e1c20c64cbe427caaaddccc11814e28ae621cd
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2020-08-12 16:21:03 +0200

    Refactor tests.

--- sentry-logback/src/test/kotlin/io/sentry/logback/SentryAppenderTest.kt
+++ sentry-logback/src/test/kotlin/io/sentry/logback/SentryAppenderTest.kt
@@ -1,13 +1,12 @@
 package io.sentry.logback
 
 import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.argumentCaptor
+import com.nhaarman.mockitokotlin2.check
 import com.nhaarman.mockitokotlin2.eq
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import io.sentry.core.ISentryClient
 import io.sentry.core.Sentry
-import io.sentry.core.SentryEvent
 import io.sentry.core.SentryLevel
 import kotlin.test.Ignore
 import kotlin.test.Test
@@ -20,7 +19,6 @@ import org.slf4j.MDC
 class SentryAppenderTest {
     private class Fixture {
         val client = mock<ISentryClient>()
-        val captor = argumentCaptor<SentryEvent>()
         val logger: Logger = LoggerFactory.getLogger(SentryAppenderTest::class.java)
 
         init {
@@ -34,79 +32,65 @@ class SentryAppenderTest {
     fun `converts message`() {
         fixture.logger.debug("testing message conversion", 1, 2)
 
-        verify(fixture.client).captureEvent(fixture.captor.capture(), any(), eq(null))
-
-        with(fixture.captor.firstValue) {
-            assertEquals("testing message conversion", this.message.formatted)
-            assertEquals(listOf("1", "2"), this.message.params)
-            assertEquals("io.sentry.logback.SentryAppenderTest", this.logger)
-        }
+        verify(fixture.client).captureEvent(check {
+            assertEquals("testing message conversion", it.message.formatted)
+            assertEquals(listOf("1", "2"), it.message.params)
+            assertEquals("io.sentry.logback.SentryAppenderTest", it.logger)
+        }, any(), eq(null))
     }
 
     @Test
     fun `converts trace log level to Sentry level`() {
         fixture.logger.trace("testing trace level")
 
-        verify(fixture.client).captureEvent(fixture.captor.capture(), any(), eq(null))
-
-        with(fixture.captor.firstValue) {
-            assertEquals(SentryLevel.DEBUG, this.level)
-        }
+        verify(fixture.client).captureEvent(check {
+            assertEquals(SentryLevel.DEBUG, it.level)
+        }, any(), eq(null))
     }
 
     @Test
     fun `converts debug log level to Sentry level`() {
         fixture.logger.debug("testing debug level")
 
-        verify(fixture.client).captureEvent(fixture.captor.capture(), any(), eq(null))
-
-        with(fixture.captor.firstValue) {
-            assertEquals(SentryLevel.DEBUG, this.level)
-        }
+        verify(fixture.client).captureEvent(check {
+            assertEquals(SentryLevel.DEBUG, it.level)
+        }, any(), eq(null))
     }
 
     @Test
     fun `converts info log level to Sentry level`() {
         fixture.logger.info("testing info level")
 
-        verify(fixture.client).captureEvent(fixture.captor.capture(), any(), eq(null))
-
-        with(fixture.captor.firstValue) {
-            assertEquals(SentryLevel.INFO, this.level)
-        }
+        verify(fixture.client).captureEvent(check {
+            assertEquals(SentryLevel.INFO, it.level)
+        }, any(), eq(null))
     }
 
     @Test
     fun `converts warn log level to Sentry level`() {
         fixture.logger.warn("testing warn level")
 
-        verify(fixture.client).captureEvent(fixture.captor.capture(), any(), eq(null))
-
-        with(fixture.captor.firstValue) {
-            assertEquals(SentryLevel.WARNING, this.level)
-        }
+        verify(fixture.client).captureEvent(check {
+            assertEquals(SentryLevel.WARNING, it.level)
+        }, any(), eq(null))
     }
 
     @Test
     fun `converts error log level to Sentry level`() {
         fixture.logger.error("testing error level")
 
-        verify(fixture.client).captureEvent(fixture.captor.capture(), any(), eq(null))
-
-        with(fixture.captor.firstValue) {
-            assertEquals(SentryLevel.ERROR, this.level)
-        }
+        verify(fixture.client).captureEvent(check {
+            assertEquals(SentryLevel.ERROR, it.level)
+        }, any(), eq(null))
     }
 
     @Test
     fun `attaches thread information`() {
         fixture.logger.warn("testing thread information")
 
-        verify(fixture.client).captureEvent(fixture.captor.capture(), any(), eq(null))
-
-        with(fixture.captor.firstValue) {
-            assertNotNull(this.getExtra("thread_name"))
-        }
+        verify(fixture.client).captureEvent(check {
+            assertNotNull(it.getExtra("thread_name"))
+        }, any(), eq(null))
     }
 
     @Test
@@ -114,11 +98,9 @@ class SentryAppenderTest {
         MDC.put("key", "value")
         fixture.logger.warn("testing thread information")
 
-        verify(fixture.client).captureEvent(fixture.captor.capture(), any(), eq(null))
-
-        with(fixture.captor.firstValue) {
-            assertEquals("value", this.getTag("key"))
-        }
+        verify(fixture.client).captureEvent(check {
+            assertEquals("value", it.getTag("key"))
+        }, any(), eq(null))
     }
 
     @Test
@@ -126,11 +108,9 @@ class SentryAppenderTest {
         val throwable = RuntimeException("something went wrong")
         fixture.logger.warn("testing throwable", throwable)
 
-        verify(fixture.client).captureEvent(fixture.captor.capture(), any(), eq(null))
-
-        with(fixture.captor.firstValue) {
-            assertEquals(throwable, this.throwable)
-        }
+        verify(fixture.client).captureEvent(check {
+            assertEquals(throwable, it.throwable)
+        }, any(), eq(null))
     }
 
     @Test
@@ -138,11 +118,9 @@ class SentryAppenderTest {
     fun `sets sdk version name`() {
         fixture.logger.info("testing sdk version")
 
-        verify(fixture.client).captureEvent(fixture.captor.capture(), any(), eq(null))
-
-        with(fixture.captor.firstValue) {
-            assertEquals(BuildConfig.SENTRY_LOGBACK_SDK_NAME, this.sdk.name)
-            assertEquals(BuildConfig.VERSION_NAME, this.sdk.version)
-        }
+        verify(fixture.client).captureEvent(check {
+            assertEquals(BuildConfig.SENTRY_LOGBACK_SDK_NAME, it.sdk.name)
+            assertEquals(BuildConfig.VERSION_NAME, it.sdk.version)
+        }, any(), eq(null))
     }
 }
