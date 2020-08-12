b672bbaa62c5d81bd0155a968a2ff1646e037435
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2020-08-12 14:39:13 +0200

    Remove unwanted accidental changes.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,8 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     `kotlin-dsl`
-    kotlin("jvm") version "1.3.72"
 }
 
 repositories {
@@ -12,15 +9,4 @@ repositories {
 // Disabling the warning about the use of experimental Kotlin compiler features
 kotlinDslPluginOptions {
     experimentalWarning.set(false)
-}
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-}
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
 }
