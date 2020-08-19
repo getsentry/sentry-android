b7124b476d657e92fb64fce06d6f1ed4980551e2
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2020-08-19 11:14:25 +0200

    Fix tests.

--- sentry-logback/src/test/kotlin/io/sentry/logback/SentryAppenderTest.kt
+++ sentry-logback/src/test/kotlin/io/sentry/logback/SentryAppenderTest.kt
@@ -11,6 +11,7 @@ import io.sentry.core.SentryEvent
 import io.sentry.core.SentryLevel
 import io.sentry.core.transport.ITransport
 import io.sentry.core.transport.TransportResult
+import org.awaitility.kotlin.await
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
@@ -54,66 +55,80 @@ class SentryAppenderTest {
     fun `converts message`() {
         fixture.logger.debug("testing message conversion {}, {}", 1, 2)
 
-        verify(fixture.transport).send(check { it: SentryEvent ->
-            assertEquals("testing message conversion 1, 2", it.message.formatted)
-            assertEquals("testing message conversion {}, {}", it.message.message)
-            assertEquals(listOf("1", "2"), it.message.params)
-            assertEquals("io.sentry.logback.SentryAppenderTest", it.logger)
-        })
+        await.untilAsserted {
+            verify(fixture.transport).send(check { it: SentryEvent ->
+                assertEquals("testing message conversion 1, 2", it.message.formatted)
+                assertEquals("testing message conversion {}, {}", it.message.message)
+                assertEquals(listOf("1", "2"), it.message.params)
+                assertEquals("io.sentry.logback.SentryAppenderTest", it.logger)
+            })
+        }
     }
 
     @Test
     fun `converts trace log level to Sentry level`() {
         fixture.logger.trace("testing trace level")
 
-        verify(fixture.transport).send(check { it: SentryEvent ->
-            assertEquals(SentryLevel.DEBUG, it.level)
-        })
+        await.untilAsserted {
+            verify(fixture.transport).send(check { it: SentryEvent ->
+                assertEquals(SentryLevel.DEBUG, it.level)
+            })
+        }
     }
 
     @Test
     fun `converts debug log level to Sentry level`() {
         fixture.logger.debug("testing debug level")
 
-        verify(fixture.transport).send(check { it: SentryEvent ->
-            assertEquals(SentryLevel.DEBUG, it.level)
-        })
+        await.untilAsserted {
+            verify(fixture.transport).send(check { it: SentryEvent ->
+                assertEquals(SentryLevel.DEBUG, it.level)
+            })
+        }
     }
 
     @Test
     fun `converts info log level to Sentry level`() {
         fixture.logger.info("testing info level")
 
-        verify(fixture.transport).send(check { it: SentryEvent ->
-            assertEquals(SentryLevel.INFO, it.level)
-        })
+        await.untilAsserted {
+            verify(fixture.transport).send(check { it: SentryEvent ->
+                assertEquals(SentryLevel.INFO, it.level)
+            })
+        }
     }
 
     @Test
     fun `converts warn log level to Sentry level`() {
         fixture.logger.warn("testing warn level")
 
-        verify(fixture.transport).send(check { it: SentryEvent ->
-            assertEquals(SentryLevel.WARNING, it.level)
-        })
+        await.untilAsserted {
+            verify(fixture.transport).send(check { it: SentryEvent ->
+                assertEquals(SentryLevel.WARNING, it.level)
+            })
+        }
     }
 
     @Test
     fun `converts error log level to Sentry level`() {
         fixture.logger.error("testing error level")
 
-        verify(fixture.transport).send(check { it: SentryEvent ->
-            assertEquals(SentryLevel.ERROR, it.level)
-        })
+        await.untilAsserted {
+            verify(fixture.transport).send(check { it: SentryEvent ->
+                assertEquals(SentryLevel.ERROR, it.level)
+            })
+        }
     }
 
     @Test
     fun `attaches thread information`() {
         fixture.logger.warn("testing thread information")
 
-        verify(fixture.transport).send(check { it: SentryEvent ->
-            assertNotNull(it.getExtra("thread_name"))
-        })
+        await.untilAsserted {
+            verify(fixture.transport).send(check { it: SentryEvent ->
+                assertNotNull(it.getExtra("thread_name"))
+            })
+        }
     }
 
     @Test
@@ -121,9 +136,11 @@ class SentryAppenderTest {
         MDC.put("key", "value")
         fixture.logger.warn("testing thread information")
 
-        verify(fixture.transport).send(check { it: SentryEvent ->
-            assertEquals(mapOf("key" to "value"), it.contexts["MDC"])
-        })
+        await.untilAsserted {
+            verify(fixture.transport).send(check { it: SentryEvent ->
+                assertEquals(mapOf("key" to "value"), it.contexts["MDC"])
+            })
+        }
     }
 
     @Test
@@ -131,23 +148,27 @@ class SentryAppenderTest {
         val throwable = RuntimeException("something went wrong")
         fixture.logger.warn("testing throwable", throwable)
 
-        verify(fixture.transport).send(check { it: SentryEvent ->
-            assertEquals(throwable, it.throwable)
-        })
+        await.untilAsserted {
+            verify(fixture.transport).send(check { it: SentryEvent ->
+                assertEquals(throwable, it.throwable)
+            })
+        }
     }
 
     @Test
     fun `sets SDK version`() {
         fixture.logger.info("testing sdk version")
 
-        verify(fixture.transport).send(check { it: SentryEvent ->
-            assertEquals(BuildConfig.SENTRY_LOGBACK_SDK_NAME, it.sdk.name)
-            assertEquals(BuildConfig.VERSION_NAME, it.sdk.version)
-            assertNotNull(it.sdk.packages)
-            assertTrue(it.sdk.packages!!.any { pkg ->
-                "maven:sentry-logback" == pkg.name
-                    && BuildConfig.VERSION_NAME == pkg.version
+        await.untilAsserted {
+            verify(fixture.transport).send(check { it: SentryEvent ->
+                assertEquals(BuildConfig.SENTRY_LOGBACK_SDK_NAME, it.sdk.name)
+                assertEquals(BuildConfig.VERSION_NAME, it.sdk.version)
+                assertNotNull(it.sdk.packages)
+                assertTrue(it.sdk.packages!!.any { pkg ->
+                    "maven:sentry-logback" == pkg.name
+                        && BuildConfig.VERSION_NAME == pkg.version
+                })
             })
-        })
+        }
     }
 }
