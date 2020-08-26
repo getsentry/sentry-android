cb0a1a93259c30ac2be601d442ae2b84405189b9
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2020-08-26 13:50:14 +0200

    Polish Gradle configuration.

--- sentry-samples/sentry-samples-spring-boot/build.gradle.kts
+++ sentry-samples/sentry-samples-spring-boot/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.3.3.RELEASE"
-    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    id(Config.BuildPlugins.springBoot) version Config.springBootVersion
+    id(Config.BuildPlugins.springDependencyManagement) version Config.BuildPlugins.springDependencyManagementVersion
     kotlin("jvm")
-    kotlin("plugin.spring") version "1.3.72"
+    kotlin("plugin.spring") version Config.kotlinVersion
 }
 
 group = "io.sentry.sample.spring-boot"
