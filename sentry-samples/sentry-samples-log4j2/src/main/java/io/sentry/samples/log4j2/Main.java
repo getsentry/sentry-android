b6c9ce73be6a9de5a1f6e98635cdbdb9bad7b4d9
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2020-09-01 09:19:19 +0200

    Add Log4j2 integration.

--- sentry-samples/sentry-samples-log4j2/src/main/java/io/sentry/samples/log4j2/Main.java
+++ sentry-samples/sentry-samples-log4j2/src/main/java/io/sentry/samples/log4j2/Main.java
@@ -1,18 +1,18 @@
-package io.sentry.samples.logback;
+package io.sentry.samples.log4j2;
 
 import java.util.UUID;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
 
 public class Main {
-  private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+  private static final Logger LOGGER = LogManager.getLogger(Main.class);
 
   public static void main(String[] args) {
     LOGGER.debug("Hello Sentry!");
 
     // MDC parameters are converted to Sentry Event tags
-    MDC.put("userId", UUID.randomUUID().toString());
+    ThreadContext.put("userId", UUID.randomUUID().toString());
 
     // logging arguments are converted to Sentry Event parameters
     LOGGER.info("User has made a purchase of product: {}", 445);
