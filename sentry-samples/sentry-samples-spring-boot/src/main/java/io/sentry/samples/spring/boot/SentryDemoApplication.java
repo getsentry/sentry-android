3d03787ec8e288f0803fe786a22823a591505326
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2020-09-04 10:59:24 +0200

    Polish.

--- sentry-samples/sentry-samples-spring-boot/src/main/java/io/sentry/samples/spring/boot/SentryDemoApplication.java
+++ sentry-samples/sentry-samples-spring-boot/src/main/java/io/sentry/samples/spring/boot/SentryDemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SentryDemoApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(SentryDemoApplication.class, args);
   }
--- sentry-samples/sentry-samples-spring/src/main/java/io/sentry/samples/spring/SentryDemoApplication.java
+++ sentry-samples/sentry-samples-spring/src/main/java/io/sentry/samples/spring/SentryDemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @EnableSentry
 public class SentryDemoApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(SentryDemoApplication.class, args);
   }
