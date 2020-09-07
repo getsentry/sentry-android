b98827f4f632686bb78ec99d5f778bc0f6d84cea
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2020-09-07 08:42:39 +0200

    Set Java version and vendor in custom event processor in sample app.
    Currently there is no option to set this on the runtime as Sentry does not recognize Java runtime.

--- sentry-samples/sentry-samples-spring-boot/src/main/java/io/sentry/samples/spring/boot/CustomEventProcessor.java
+++ sentry-samples/sentry-samples-spring-boot/src/main/java/io/sentry/samples/spring/boot/CustomEventProcessor.java
@@ -11,9 +11,22 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class CustomEventProcessor implements EventProcessor {
+  private final String javaVersion;
+  private final String javaVendor;
+
+  public CustomEventProcessor(String javaVersion, String javaVendor) {
+    this.javaVersion = javaVersion;
+    this.javaVendor = javaVendor;
+  }
+
+  public CustomEventProcessor() {
+    this(System.getProperty("java.version"), System.getProperty("java.vendor"));
+  }
+
   @Override
   public SentryEvent process(SentryEvent event, @Nullable Object hint) {
-    event.setTag("Java Version", System.getProperty("java.version"));
+    event.setTag("Java-Version", javaVersion);
+    event.setTag("Java-Vendor", javaVendor);
     return event;
   }
 }
