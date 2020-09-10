29f0b15e241b4bf0732e23f14bcb48f09a5399b4
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2020-09-10 20:24:34 +0200

    Polish.

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -5,6 +5,7 @@ object Config {
     val kotlinStdLib = "stdlib-jdk8"
 
     val springBootVersion = "2.3.3.RELEASE"
+    // Spring is currently not compatible with Kotlin 1.4
     val springKotlinCompatibleLanguageVersion = "1.3"
 
     object BuildPlugins {
